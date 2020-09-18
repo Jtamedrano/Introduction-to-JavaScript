d6bae93b29b81281c1f398a3dcd471c3e07a9326
Jesse Medrano <Jtamedrano@gmail.com>
2020-09-18 13:36:23 -0700

    Completed all task + stretch

--- index.js
+++ index.js
@@ -97,16 +97,113 @@ console.log(dogFeeder(dogAge(1, 0), 15));
 // use math.random to determine the computers choice
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number
 
+function convertToString(choice) {
+  switch (choice) {
+    case 1:
+      return "Rock";
+    case 2:
+      return "Paper";
+    case 3:
+      return "Scissors";
+  }
+}
+
+function playerChoice(choice) {
+  choice = choice.charAt(0);
+  switch (choice) {
+    case "r":
+      return 1;
+    case "p":
+      return 2;
+    case "s":
+      return 3;
+    case "c":
+      let x = Math.ceil(Math.random() * 3);
+
+      return x;
+  }
+}
+
+function outcome(one, two) {
+  if (one == two) {
+    return "We have a Tie.\n";
+  } else {
+    switch (one) {
+      case 1:
+        if (two == 2) {
+          return "Player Two Wins!\n";
+        } else {
+          return "Player One Wins!\n";
+        }
+      case 2:
+        if (two == 3) {
+          return "Player Two Wins!\n";
+        } else {
+          return "Player One Wins!\n";
+        }
+      case 3:
+        if (two == 1) {
+          return "Player Two Wins!\n";
+        } else {
+          return "Player One Wins!\n";
+        }
+    }
+  }
+}
+
+function playGameConsole(playerOne, playerTwo) {
+  let playerOneChoice = playerChoice(playerOne.toLowerCase());
+  let playerTwoChoice = playerChoice(playerTwo.toLowerCase());
+
+  console.log(
+    outcome(playerOneChoice, playerTwoChoice) +
+      `Player One chose ${convertToString(
+        playerOneChoice
+      )} and Player Two chose ${convertToString(playerTwoChoice)}`
+  );
+}
+
+playGameConsole("rock", "rock");
+playGameConsole("rock", "computer");
+
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
 //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
 
+function kmToMiles(km) {
+  return km / 1.609;
+}
+
+console.log(kmToMiles(1));
+console.log(kmToMiles(2));
+console.log(kmToMiles(5));
+
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
 
+function ftToCm(ft) {
+  return ft * 30.48;
+}
+
+console.log(ftToCm(1));
+console.log(ftToCm(2));
+console.log(ftToCm(5));
 /************************************************************** Task 6 **************************************************************/
 // 99 bottles of soda on the wall
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
+
+function annoyingSong(numb) {
+  for (i = numb; i > 0; i--) {
+    console.log(
+      `${numb} bottles of soda on the wall, ${numb} bottles of soda, take one down pass it around ${
+        numb - 1
+      } bottles of soda on the wall.`
+    );
+    numb--;
+  }
+}
+
+console.log(annoyingSong(3));
 
 /************************************************************** Task 7 **************************************************************/
 //Grade Calculator
@@ -117,11 +214,76 @@ console.log(dogFeeder(dogAge(1, 0), 15));
 //60s should be D
 //and anything below 60 should be F
 
+function gradeCalc(score) {
+  if (score < 60) {
+    return "F";
+  } else if (score >= 60 && score < 70) {
+    return "D";
+  } else if (score >= 70 && score < 80) {
+    return "C";
+  } else if (score >= 80 && score < 90) {
+    return "B";
+  } else {
+    return "A";
+  }
+}
+
+console.log(gradeCalc(100));
+console.log(gradeCalc(85));
+console.log(gradeCalc(75));
+console.log(gradeCalc(65));
+console.log(gradeCalc(49));
+
 /************************************************************** Stretch **************************************************************/
 //Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.
 // Hint - you may need to study tomorrow's traning kit on arrays
 // try looking up the .includes() method
 
+function countVowelsOnly(string) {
+  let stringArray = string.toLowerCase().split("");
+  let vowelCount = 0;
+  stringArray.forEach((element) => {
+    const vowels = ["a", "e", "i", "o", "u", "y"];
+    if (vowels.includes(element)) {
+      vowelCount++;
+    }
+  });
+  return vowelCount;
+}
+
+console.log(countVowelsOnly("Hello!"));
+console.log(countVowelsOnly("sUpercAlifrAgilisticExpialidocious"));
+
 /************************************************************** Stretch **************************************************************/
 //Take Rock, Paper, Sissors further
 //update your rock papers sissors code below to take a prompt from a user using the window object
+
+function playGamePrompt(playerOne, playerTwo) {
+  let playerOneChoice = playerChoice(playerOne.toLowerCase());
+  let playerTwoChoice = playerChoice(playerTwo.toLowerCase());
+
+  window.alert(
+    outcome(playerOneChoice, playerTwoChoice) +
+      `Player one chose ${convertToString(
+        playerOneChoice
+      )} and player two chose ${convertToString(playerTwoChoice)}.`
+  );
+}
+
+function initGame() {
+  let playerTwo = window.prompt(
+    "Do you want to play against another player locally? (y/n)\n(choosing 'n' will play the computer)"
+  );
+  let gameScript = "(R)ock, (P)aper or (S)cissors?";
+  switch (playerTwo.toLowerCase()) {
+    case "n":
+      let userChoice = window.prompt(gameScript);
+      playGamePrompt(userChoice.toLowerCase(), "computer");
+      break;
+    case "y":
+      let userChoiceOne = window.prompt("Player 1: " + gameScript);
+      let userChoiceTwo = window.prompt("Player 2: " + gameScript);
+      playGamePrompt(userChoiceOne.toLowerCase(), userChoiceTwo.toLowerCase());
+      break;
+  }
+}
